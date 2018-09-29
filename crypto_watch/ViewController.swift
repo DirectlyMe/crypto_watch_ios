194da178fd8a5b9bb655759c403ef8f685abb68d
Jack Hansen <jack@Jacks-MacBook-Pro.local>
2018-09-29 14:06:05 -0600

    Initial Commit

---
+++ crypto_watch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  crypto_watch
+//
+//  Created by Jack Hansen on 9/29/18.
+//  Copyright © 2018 Jack Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
